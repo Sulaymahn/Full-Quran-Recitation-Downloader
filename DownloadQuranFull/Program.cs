da917337aa205eed43b3385ad11c7fda57282083
Sulaymahn <sulmuk28@gmail.com>
2022-09-26 15:33:04 +0100

    Downloads to local download folder

--- DownloadQuranFull/Program.cs
+++ DownloadQuranFull/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -7,16 +8,19 @@ namespace DownloadQuranFull
 {
     class Program
     {
+        static string urirootpath = "https://download.quranicaudio.com/quran";
+        static string localfoldername = "Yasser Ad-dussary";
         static void Main(string[] args)
         {
             WebClient client = new WebClient();
 
             string[] links = GetLinks();
-
             int counter = 1;
+
+            var dir = Directory.CreateDirectory(Path.Combine(KnownFolders.GetPath(KnownFolder.Downloads), localfoldername));
             foreach (var link in links)
             {
-                client.DownloadFile(new Uri(link), "Surah" + link.Substring(link.LastIndexOf('/') + 1));
+                client.DownloadFile(new Uri(link), Path.Combine(dir.FullName, link.Substring(link.LastIndexOf('/') + 1)));
                 Console.WriteLine("Downloaded Surah " + counter);
                 counter++;
             }
@@ -24,7 +28,7 @@ namespace DownloadQuranFull
         static string[] GetLinks()
         {
             string[] links = new string[114];
-            string baselink = "https://download.quranicaudio.com/quran/yasser_ad-dussary/";
+            string baselink = string.Concat(urirootpath, "/yasser_ad-dussary/");
             for (int i = 0; i < 114; i++)
             {
                 links[i] = baselink + CovertToThreeDigit(i + 1) + ".mp3";
